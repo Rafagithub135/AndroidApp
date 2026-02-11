69cd6bea73d3cc12606eafdcf1f8ef8d14d95cae
rafagithub135 <mccloskey.ralph35@gmail.com>
2026-02-11 09:32:19 -0500

    Created initial program with kotlin.

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    print("Enter the price of the item:  ")
+    val price = readLine()!!.toDouble()
+    println("Enter the quantity of the item:  ")
+    val quantity = readLine()!!.toInt()
+    println("The total cost is:  " + (price * quantity))
+}
